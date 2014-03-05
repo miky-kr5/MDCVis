fbb46ad8f8dea070f893db2223d24d5228b1c21d
unknown <miguel.astor@CICORE215.icaro.ciens.ucv.ve>
2014-03-05 13:35:33 -0430

    Added windows compilation files.

--- Windows.mk
+++ Windows.mk
@@ -2,5 +2,5 @@ COMPILER = g++
 FLAGS = -Wall
 INCLUDE = -I./include
 LIBDIRS = -L./lib -L./lib/Windows 
-LIBS = -lIrrlicht -lopengl32 -lsqlite3 -lpthread -ldl
+LIBS = -lIrrlicht -lopengl32 -lsqlite3 -lpthreadGC2 -ldl
 TARGET = bin/MDCVis.exe
